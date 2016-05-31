a9476198f4398a8f9c540090a66ff36b36d2b65c
jrizza88 <jetorres88@gmail.com>
2016-05-30 21:15:51 -0400

    fixed a bug in the code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,58 +7,61 @@ var Letters = [
 // scoreboard counters and containers
 var userLose = 0;
 var userWin = 0;
-var guessesLeft = 13;
-var guessed = [];
+var GuessesLeft = 13;
+var Guessed = [];
+var reset = function() {
+        GuessesLeft = 13;
+        Guessed = [];
+        }
 
 // Tally total and displaying them in HTML 
 "<p>wins: " +  userWin + "</p>" +
 "<p>losses: " + userLose +  "</p>";
 
-
-// Verify user input against allowedLetters array
-
-  // sets computer choicess
-  
-
-  // This sets the computer guess equal to the random.
-
-  // When the user presses the key it records the keypress and then sets it to userguess
-  document.onkeyup = function guessChoices(event) {
+// When the user presses the key it records the keypress and then sets it to userguess
+  document.onkeyup = function(event) {
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-    var computerChoices = Math.random();
-    var options = Math.floor(computerChoices*Letters.length);
-    var compGuess = computerChoices[options];
+    var compGuess = Letters[Math.floor(Math.random() * Letters.length)];
+
+console.log('the user pressed' + event.keyCode)
  
 // user chooses letter
+
+if (Letters.indexOf(userGuess) > -1){
+  console.log("Correct guess!");
+  document.getElementbyID("wins").innerHTML= "<p>You are correct! whoop whoop!</p>" + 
+  "<p>wins: " + win + "</p>" + 
+  "<p>losses: " + lose + "</p>";
+  Guessed.push(userGuess);
+  GuessesLeft--;
+
+}
+else {
+  console.log("Not a valid input!")
+}
+
 if (compGuess === userGuess) {
   userWin++;
-// for winning choices
-  documentgetElementbyID("wins").innerHTML= "<p>You are correct! whoop whoop!</p>" + 
-    "<p>wins: " + win + "</p>" + 
-    "<p>losses: " + lose + "</p>";
-
-//for tracking losing choices
-}else {
-  userLose++;
-  document.getElementbyID("#wins").innerHTML ="<p>Wrong! Try again </p>" +  
-    "<p>wins</p>" + win + "</p>" + 
-    "<p>losses: "  + lose + "</p>";
+  reset();
 }
-// Game over condition set
-  if (guessesLeft===13) {
-    document.getElementbyID("#wins").innerHTML="<p>You're out of guesses! Try again mate!</p>";
-  }
-};
+
+//Game reset condition for "game over"
+if(GuessesLeft == 0){
+  userLose++;
+  document.getElementbyID("wins").innerHTML="<p>You're out of guesses! Try again mate!</p>";
+  reset();
+}
   // counters and containers
  
-        console.log("total wins: " + userWin);
-        console.log("total losses: " + userLose);
-        console.log("guesses left: "+ guessesLeft);
+  console.log("total wins: " + userWin);
+  console.log("total losses: " + userLose);
+  console.log("Guesses left: "+ GuessesLeft);
+  console.log("Guessed: " + Guessed);
 
-        document.querySelector('#wins').innerHTML=
-        '<p>Wins: ' + userWin + '</p>' +
-        '<p>Losses: ' + userLose + '</p>' +
-        '<p>Guesses Left: ' + guessesLeft + '</p>';
+  document.querySelector('#wins').innerHTML=
+  '<p>Wins: ' + userWin + '</p>' +
+  '<p>Losses: ' + userLose + '</p>' +
+  '<p>Guesses Left: ' + GuessesLeft + '</p>';
 
-
+};
 
