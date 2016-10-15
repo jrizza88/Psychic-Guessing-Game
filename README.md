c62d5989be2533aadd4438cef9fbdd3c764e13ae
jrizza88 <jetorres88@gmail.com>
2016-10-14 21:12:24 -0400

    updated first JS hw assignment

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # week-3-game
-hangman homework project
+
+First Javascript homework assignment. 
+
+It's the psychic guessing game. You have to guess which letter that is pre-determined by the code. If you match the pre-determined number, you win the game. If not, you lose the game. 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-// Defining variables
+6// Defining variables
 var Letters = [
   'a','b','c','d','e','f','g','h','i','j','k',
   'l','m','n','o','p','q','r','s','t','u','v',
@@ -12,56 +12,53 @@ var Guessed = [];
 var reset = function() {
         GuessesLeft = 13;
         Guessed = [];
-        }
+        };
 
-// Tally total and displaying them in HTML 
-"<p>wins: " +  userWin + "</p>" +
-"<p>losses: " + userLose +  "</p>";
-
-// When the user presses the key it records the keypress and then sets it to userguess
-  document.onkeyup = function(event) {
+// When the user presses the key it records the keypress and then sets it to userGuess
+    document.onkeyup = function(event) {
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
     var compGuess = Letters[Math.floor(Math.random() * Letters.length)];
 
-console.log('the user pressed' + event.keyCode)
- 
-// user chooses letter
-
-if (Letters.indexOf(userGuess) > -1){
-  console.log("Correct guess!");
-  document.getElementbyID("wins").innerHTML= "<p>You are correct! whoop whoop!</p>" + 
-  "<p>wins: " + win + "</p>" + 
-  "<p>losses: " + lose + "</p>";
-  Guessed.push(userGuess);
-  GuessesLeft--;
-
-}
-else {
-  console.log("Not a valid input!")
-}
-
-if (compGuess === userGuess) {
-  userWin++;
-  reset();
-}
-
-//Game reset condition for "game over"
-if(GuessesLeft == 0){
-  userLose++;
-  document.getElementbyID("wins").innerHTML="<p>You're out of guesses! Try again mate!</p>";
-  reset();
-}
-  // counters and containers
- 
-  console.log("total wins: " + userWin);
-  console.log("total losses: " + userLose);
-  console.log("Guesses left: "+ GuessesLeft);
-  console.log("Guessed: " + Guessed);
-
-  document.querySelector('#wins').innerHTML=
-  '<p>Wins: ' + userWin + '</p>' +
-  '<p>Losses: ' + userLose + '</p>' +
-  '<p>Guesses Left: ' + GuessesLeft + '</p>';
-
-};
-
+        console.log('the user pressed' + event.keyCode)
+         
+        // user chooses letter
+        
+        if (Letters.indexOf(userGuess) > -1){
+          console.log("Correct guess!");
+          document.getElementById('wins').innerHTML= "<p>You are correct! whoop whoop!</p>" + 
+          "<p>wins: " + userWin + "</p>" + 
+          "<p>losses: " + userLose + "</p>";
+          Guessed.push(userGuess);
+          GuessesLeft--;
+        }
+        else {
+          console.log("Not a valid input!")
+        }
+        
+        if (compGuess === userGuess) {
+          userWin++;
+          reset();
+        }
+        
+        
+        //Game reset condition for "game over"
+        if(GuessesLeft == 0){
+          userLose++;
+          document.getElementById("wins").innerHTML="<p>You're out of guesses! Try again mate!</p>";
+          reset();
+        }
+          // counters and containers
+         
+          console.log("total wins: " + userWin);
+          console.log("total losses: " + userLose);
+          console.log("Guesses left: "+ GuessesLeft);
+          console.log("Guessed: " + Guessed);
+        
+          document.querySelector('#wins').innerHTML=
+          '<p>Wins: ' + userWin + '</p>' +
+          '<p>Losses: ' + userLose + '</p>' +
+          '<p>Guesses Left: ' + GuessesLeft + '</p>';
+        
+        };
+        
+        
